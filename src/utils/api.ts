6e82fa802bcb80b1f18449e39d8e98f0bfd4e2fd
Fittra Marga Ardana <fittracakomoli6@gmail.com>
2025-12-23 14:59:26 +0700

    feat: api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,5 @@
 const API_BASE_URL: string =
-    import.meta.env.VITE_API_BASE_URL ?? "http://localhost:8000";
+    import.meta.env.VITE_API_BASE_URL ?? "https://api-sbox.vercel.app";
 
 export const apiEndpoints = {
     status: `${API_BASE_URL}/status`,
